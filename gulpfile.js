095859aad7c37a57ec5720721a52fe41fc3b1a95
Evi-ava <dedasertyk.fvhg@gmail.com>
2023-05-21 02:16:38 +0300

    Тест компиляции компонентов

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,46 @@ const concat = require('gulp-concat');
 const map    = require('gulp-sourcemaps');
 // const chalk  = require('chalk');
 
-const freshFramework = "/fresh/fresh-framework/*.scss";
-const freshCSS = "/fresh/fresh-css/";
+const freshFramework = "./fresh/fresh-framework/fresh.scss";
+const freshComponensts = "./fresh/fresh-components/**/*.scss"
+const freshCSS = "./fresh/fresh-css/";
 
-const scssHandler = () => {
-    return src(freshFramework)
+function scssHandler() {
+    return gulp.src(freshFramework)
     .pipe(map.init())
     .pipe(sass({
-      outputStyle: 'compressed'
+      // outputStyle: 'compressed'
+         outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(concat('libs.min.css'))
+    .pipe(concat('fresh.min.css'))
     .pipe(map.write('../sourcemaps/'))
-    .pipe(dest(freshCSS))
+    .pipe(gulp.dest(freshCSS))
+}
+
+function scssComponentHandler() {
+  return gulp.src(freshComponensts)
+  .pipe(map.init())
+  .pipe(sass({
+    // outputStyle: 'compressed'
+       outputStyle: 'expanded'
+  }).on('error', sass.logError))
+  .pipe(concat('fresh.min.css'))
+  .pipe(map.write('../sourcemaps/'))
+  .pipe(gulp.dest(freshComponensts))
+}
+
+const watch_dev = () => {
+  gulp.watch(['./fresh/fresh-framework/**/*.scss'], scssHandler).on(
+    'change',
+    () => {}
+  )
+
+  gulp.watch(['./fresh/fresh-components/**/*.scss'], scssComponentHandler).on(
+    'change',
+    () => {}
+  )
 }
 
 exports.scssHandler = scssHandler;
+exports.watch_dev = watch_dev;
 
